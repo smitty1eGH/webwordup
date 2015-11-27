add97b8dd4ba3164eb42ceeba61af29ab528dee4
unknown <christosmith@USARRCHRISTOSM3.us.deloitte.com>
2015-11-26 20:04:07 -0500

    Finished tutorial

--- webwordup.py
+++ webwordup.py
@@ -21,5 +21,74 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def init_db():
+    """Created via 
+(venv) C:\Users\christosmith\Documents\proj\venv\webwordup
+>sqlite3 webwordup.db < schema.sql
+
+subsequently 
+    """
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+        
+@app.route('/')
+def show_entries():
+    cur     = g.db.execute('select entry_title, entry_text from entries order by entry_id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (entry_title, entry_text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
+
+
